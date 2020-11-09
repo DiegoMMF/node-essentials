09a08e864a19acb9f61a60caa26582cadd7790bc
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-09 12:51:58 -0300

    debugging

--- nodejs-debug/currency.js
+++ nodejs-debug/currency.js
@@ -9,8 +9,23 @@ function setExchangeRate(rate, sourceCurrency, targetCurrency) {
     rates[targetCurrency] = {};
   }
 
-  rates[sourceCurrency][targetCurrency] = rate;
-  rates[targetCurrency][sourceCurrency] = 1 / rate;
+  /**
+   * Original lines of code (before debugging tutorial):
+   * 
+   *   rates[sourceCurrency][targetCurrency] = rate;
+   *   rates[targetCurrency][sourceCurrency] = 1 / rate;
+   * 
+   * The ones that we added:
+   */
+
+  for (const currency in rates) {
+    if (currency !== targetCurrency) {
+      // use a pivot for currencies for which don't have the direct rate
+      const pivotRate = currency === sourceCurrency ? 1 : rates[currency][sourceCurrency];
+      rates[currency][targetCurrency] = rate * pivotRate;
+      rates[targetCurrency][currency] = 1 / (rate * pivotRate);
+    }
+  }
 }
 
 function convertToCurrency(value, sourceCurrency, targetCurrency) {
@@ -27,13 +42,20 @@ function printForeignValues(value, sourceCurrency) {
 
   for (const targetCurrency in rates) {
     if (targetCurrency !== sourceCurrency) {
-      const convertedValue = convertToCurrency(value, sourceCurrency, targetCurrency);
+      const convertedValue = convertToCurrency(
+        value,
+        sourceCurrency,
+        targetCurrency
+      );
       const displayValue = formatValueForDisplay(convertedValue);
-      console.info(`- ${convertedValue} ${targetCurrency}`);
+      console.info(`- ${displayValue} ${targetCurrency}`);
+      /**
+       * original line of code -> console.info(`- ${convertedValue} ${targetCurrency}`); <- before debugging
+       */ 
     }
   }
 }
 
-setExchangeRate(0.88, 'USD', 'EUR');
-setExchangeRate(107.4, 'USD', 'JPY');
-printForeignValues(10, 'EUR');
+setExchangeRate(0.88, "USD", "EUR");
+setExchangeRate(107.4, "USD", "JPY");
+printForeignValues(10, "EUR");
